d9eaf5e918d6dc061f26675cfd0bc43e73693624
Mohammad Rad <argo.mohrad@gmail.com>
2026-07-26 14:23:31 -0700

    add ble_beacon skeleton

---
+++ ble-examples/ble_beacon/main/main.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+/* Includes */
+#include "common.h"
+#include "gap.h"
+
+
+void app_main(void)
+{
+
+}
